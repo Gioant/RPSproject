30a5b14cf27c7e9220619ca8f68328c9a9f8dc91
Anthony <gioant0629@gmail.com>
2022-06-16 17:55:06 -0400

    created a function that takes 2 parameters: The player's choice and Computers choice. Function to be used for later

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 function computerPlay(){
     //generate a random number between 1-3
     let random = Math.floor(Math.random() * 3) + 1;
+    //display random number to console
     console.log("random number is: " + random);
+    // if conditions that returns either rock,paper or scissors depending random number generated
     if(random === 1){
         return console.log('Rock');
     } else if (random === 2){
@@ -11,4 +13,11 @@ function computerPlay(){
     }
 }
 
+//call function
 computerPlay();
+
+
+//function to play a single round of the game
+function playRound(playerSelection, computerSelection){
+    
+}
