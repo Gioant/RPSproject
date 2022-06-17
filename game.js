d7143d53d3c9fd6af47a64af49589f7b56b81846
Anthony <gioant0629@gmail.com>
2022-06-17 13:25:57 -0400

    added a function for playerSelection. This will allow a user to enter an input... and only if its a valid one will it be used for playRound function

--- game.js
+++ game.js
@@ -20,6 +20,37 @@ function computerPlay(){
     }
 }
 
+function playerPlay(){
+    var valid = false;
+
+    while (!valid){
+        //prompt user for choice
+        let input = prompt("Enter Rock, Paper or Scissors");
+        
+        //if user's input is not empty
+        if (input.trim() != null){
+            //make their input all lowercase
+            var playerChoice = input.toLowerCase();
+
+            //verify if the playerChoice is a valid choice
+            if(playerChoice != 'rock' || playerChoice != 'paper' || playerChoice != 'scissors'){
+                //display alert err msg
+                alert("Incorrect Input. Please Enter A Valid Choice From Prompt.");
+                valid = false;
+            } else {
+                //else input is valid!
+                console.log(playerChoice); // console log the value
+                valid = true;
+                //break out of loop just in case
+                break;
+            }
+        } else {
+            alert("Invalid Input! Input Cannot be Empty.");
+        }
+    }
+    //return value 
+    return playerChoice;
+}
 
 // to play a single round of the game
 function playRound(playerSelection, computerSelection) {
@@ -28,12 +59,13 @@ function playRound(playerSelection, computerSelection) {
     let lose = "You Lose. Paper Beats Rock";
 
     if(playerSelection == computerSelection){
-        return draw;
+        return console.log(draw);
     } else if (computerSelection == 'paper') {
     }
 }
 
-const computerSelection = computerPlay();
+const playerSelection = playerPlay();
+// const computerSelection = computerPlay();
 
 
 
