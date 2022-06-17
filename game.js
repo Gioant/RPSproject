f889abf6c7e3ebd7548c215cba11462b4388d0dc
Anthony <gioant0629@gmail.com>
2022-06-16 21:46:33 -0400

    created a do while loop to prompt user for their input and make it case-insensitive for playerSelection parameter

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
+//keep prompting user until they enter a choice
+do {
+    //prompt user for their choice
+    let input = prompt("Enter Rock, Paper or Scissors");
+    //make their choice case-insensitive so they can input
+    let playerSelection = input.toLowerCase();
+}while(input != null);
+
 function computerPlay(){
     //generate a random number between 1-3
     let random = Math.floor(Math.random() * 3) + 1;
